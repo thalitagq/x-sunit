b0b61ec87bf95405f11b4875fef9f8b3cdb8c76d
thalitag <thalitagq@outlook.com>
2018-08-05 18:01:01 -0300

    update survivors' table after update on abduction_reports

--- app/models/abduction_report.rb
+++ app/models/abduction_report.rb
@@ -1,3 +1,12 @@
 class AbductionReport < ApplicationRecord
 	belongs_to :survivor, optional: true
+	after_save :update_survivor
+	after_update :update_survivor
+
+	def update_survivor
+	survivor = Survivor.find(self.id)
+	amount = AbductionReport.find(self.id).amount	
+	survivor.update!(abduction_reports: amount)
+	end
+
 end
--- db/migrate/20180805142321_add_foreing_key.rb
+++ db/migrate/20180805142321_add_foreing_key.rb
@@ -1,5 +1,6 @@
 class AddForeingKey < ActiveRecord::Migration[5.1]
   def change
   	add_foreign_key :abduction_reports, :survivors, column:  :id, primary_key: :id
+
   end
 end
